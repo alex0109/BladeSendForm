b117c05f8e9dc8afbe75e2d4532de5d93ab95832
alex0109 <krossyouyub@gmail.com>
2020-07-14 21:33:39 +0300

    v2 of bladeForm

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alex.slep007@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'alexmonkey'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'ot_kuda.gmail@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'passwordFrom ot_kuda'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('alex.slep007@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('krossyouyub@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('ot_kuda.gmail@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('kuda.gmail@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
